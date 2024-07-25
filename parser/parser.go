bf6b3365c7f611e59ee2f75e95c00b87180f1f36
zopitty <maxtan96@gmail.com>
2024-07-26 00:09:18 +0800

    parse prefix operators with test

--- parser/parser.go
+++ parser/parser.go
@@ -46,6 +46,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	// Read two tokens, so both cur and peek are set
 	p.nextToken()
@@ -132,9 +134,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -158,6 +166,19 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+    p.nextToken()
+
+    // call parseExpression again at next token, curToken will be INT
+    expression.Right = p.parseExpression(PREFIX)
+    return expression
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
